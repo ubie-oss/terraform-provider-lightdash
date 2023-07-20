1e950e96e7bed8cdcff6ac1cb8d00bc83c4bc430
Yu ISHIKAWA <yu.ishikawa@dr-ubie.com>
2023-07-20 21:49:27 +0900

    Bump up to 0.1.0
    Signed-off-by: Yu ISHIKAWA <yu.ishikawa@dr-ubie.com>

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ import (
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary.
-	version string = "0.0.1"
+	version string = "0.1.0"
 
 	// goreleaser can pass other information to the main package, such as the specific commit
 	// https://goreleaser.com/cookbooks/using-main.version/
