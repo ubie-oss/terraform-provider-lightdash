3eeab60107bc27b87c43a2be7cafde35e825e87e
Yu ISHIKAWA <yu.ishikawa@dr-ubie.com>
2023-07-24 08:38:01 +0900

    Rename to `lightdash_space_access_member`
    Signed-off-by: Yu ISHIKAWA <yu.ishikawa@dr-ubie.com>

--- examples/resources/lightdash_space_access_member/resource.tf
+++ examples/resources/lightdash_space_access_member/resource.tf
@@ -1,4 +1,4 @@
-resource "lightdash_space_member" "example" {
+resource "lightdash_space_access_member" "example" {
   project_uuid = lightdash_space.example_public.project_uuid
   space_uuid   = lightdash_space.example_public.space_uuid
   user_uuid    = data.lightdash_organization_member.example.user_uuid
