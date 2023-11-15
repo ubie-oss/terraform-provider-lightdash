c4293f7a7a270eec0ff1b392ffe55a4ed37ff14d
Yu Ishikawa <yu.ishikawa@dr-ubie.com>
2023-11-15 09:25:29 +0900

    Fix
    Signed-off-by: Yu Ishikawa <yu.ishikawa@dr-ubie.com>

--- examples/resources/lightdash_space/resource.tf
+++ examples/resources/lightdash_space/resource.tf
@@ -1,35 +1,18 @@
 resource "lightdash_space" "test_public" {
   project_uuid = "xxxxxxxx-xxxxxxxxxx-xxxxxxxxx"
   name         = "zzz_test_private_space"
-  // The visibility is public by default.
-  // is_private   = false
+  // The visibility is private by default.
+  is_private = true
 
   deletion_protection = false
 }
 
-
-locals {
-  test_private_accessors = [
-    {
-      user_uuid = "xxxx-xxxx-xxx"
-    },
-    {
-      user_uuid = "xxxx-xxxx-xxx"
-    }
-  ]
-}
 
 resource "lightdash_space" "test_privacte" {
   project_uuid = "xxxxxxxx-xxxxxxxxxx-xxxxxxxxx"
   name         = "zzz_test_private_space"
-  is_private   = true
+  // The visibility is private by default.
+  is_private = true
 
   deletion_protection = false
-
-  dynamic "access" {
-    for_each = local.test_private_accessors
-    content {
-      user_uuid = access.value.user_uuid
-    }
-  }
 }
