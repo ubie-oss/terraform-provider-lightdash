e2287a2a1e31a77044114049f57fc03e30b351d9
Yu Ishikawa <yu.ishikawa@dr-ubie.com>
2025-05-16 17:22:59 +0900

    Update
    Signed-off-by: Yu Ishikawa <yu.ishikawa@dr-ubie.com>

--- internal/provider/resource_lightdash_project_role_group_test.go
+++ internal/provider/resource_lightdash_project_role_group_test.go
@@ -15,9 +15,11 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 // Using the shared testAccPreCheck and testAccProtoV6ProviderFactories from provider_acc_test.go
@@ -100,6 +102,73 @@ func TestAccProjectRoleGroupResource_grant(t *testing.T) {
 					// lightdash_project_role_group.resource_lightdash_project_role_group__grant
 					resource.TestCheckResourceAttr("lightdash_project_role_group.test_project_role_group", "role", "admin"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccProjectRoleGroupResource_import(t *testing.T) {
+	if !isIntegrationTestMode() {
+		t.Skip("Skipping acceptance test for resource_lightdash_project_role_group")
+	}
+
+	// Get the provider config
+	providerConfig, err := getProviderConfig()
+	if err != nil {
+		t.Fatalf("Failed to get providerConfig: %v", err)
+	}
+
+	// Test of simple space creation
+	importConfig010, err := ReadAccTestResource([]string{"resource_lightdash_project_role_group", "import", "010_import.tf"})
+	if err != nil {
+		t.Fatalf("Failed to get publicSpaceConfig: %v", err)
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + importConfig010,
+				Check: resource.ComposeTestCheckFunc(
+					// lightdash_project_role_group.resource_lightdash_project_role_group__grant
+					resource.TestCheckResourceAttrSet("lightdash_project_role_group.test_project_role_group", "project_uuid"),
+					resource.TestCheckResourceAttr("lightdash_project_role_group.test_project_role_group", "role", "viewer"),
+					resource.TestCheckResourceAttrPair(
+						"lightdash_project_role_group.test_project_role_group",
+						"group_uuid",
+						"lightdash_group.test_group",
+						"group_uuid",
+					),
+				),
+			},
+			{
+				Config:            providerConfig + importConfig010,
+				ResourceName:      "lightdash_project_role_group.test_project_role_group",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateVerifyIgnore: []string{
+					"last_updated",
+				},
+				ImportStateIdFunc: func(state *terraform.State) (string, error) {
+					res, ok := state.RootModule().Resources["lightdash_project_role_group.test_project_role_group"]
+					if !ok {
+						return "", fmt.Errorf("resource not found in state for import")
+					}
+					// Get the project_uuid from the state
+					project_uuid, ok := res.Primary.Attributes["project_uuid"]
+					if !ok && project_uuid != "" {
+						return "", fmt.Errorf("project_uuid attribute not present in state")
+					}
+					// Get the group_uuid from the state
+					group_uuid, ok := res.Primary.Attributes["group_uuid"]
+					if !ok && group_uuid != "" {
+						return "", fmt.Errorf("group_uuid attribute not present in state")
+					}
+					// Construct the import ID in the form 'projects/<project_uuid>/access-groups/<group_uuid>'
+					id := getProjectRoleGroupResourceId(project_uuid, group_uuid)
+					return id, nil
+				},
 			},
 		},
 	})
