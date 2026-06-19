a25a602c32fbc38f783b348f836d19f6a87cd223
Yu Ishikawa <yu.ishikawa@dr-ubie.com>
2026-06-19 10:06:41 +0900

    fix
    Signed-off-by: Yu Ishikawa <yu.ishikawa@dr-ubie.com>

--- integration_tests/data_source_lightdash_project_agent.tf
+++ integration_tests/data_source_lightdash_project_agent.tf
@@ -1,7 +1,7 @@
 data "lightdash_project_agent" "test" {
-  organization_uuid = data.lightdash_organization.test.organization_uuid
-  project_uuid      = data.lightdash_project.test.project_uuid
-  agent_uuid        = lightdash_project_agent.test_agent.agent_uuid
+  # organization_uuid = data.lightdash_organization.test.organization_uuid
+  project_uuid = data.lightdash_project.test.project_uuid
+  agent_uuid   = lightdash_project_agent.test_agent.agent_uuid
 }
 
 output "lightdash_project_agent_test" {
